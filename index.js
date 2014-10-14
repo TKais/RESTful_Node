1beb0fb0b41d16f4fa39ee3eb6c3533e933d14e3
TKais <TiffanyTKaiser@gmail.com>
2014-10-14 13:13:01 -0700

    Required express in index.js/made code more DRY

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-var http = require('http');
+var http = require('http'),
+    express = require('express');
  
+var app = express();
+app.set('port', process.env.PORT || 3000); 
+ 
+app.get('/', function (req, res) {
+  res.send('<html><body><h1>Hello World</h1></body></html>');
+});
+ 
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<html><body><h1>Hello World</h1></body></html>');
-}).listen(3000);
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end('<html><body><h1>Hello World</h1></body></html>');
+// }).listen(3000);
  
-console.log('Server running on port 3000.');
+// console.log('Server running on port 3000.');
