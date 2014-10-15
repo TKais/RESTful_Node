f11c7e8801daf0ca65dd8777fc0face44ce77fce
TKais <TiffanyTKaiser@gmail.com>
2014-10-15 14:22:29 -0700

    Added great example of callbacks and understanding them

--- index.js
+++ index.js
@@ -40,3 +40,34 @@ http.createServer(app).listen(app.get('port'), function(){
 //cd /usr/local/opt/mongodb/ 
 //mongod
 
+
+
+
+
+
+
+
+//GREAT EXAMPLE OF CALLBACKS
+// var fs = require('fs')
+// var myNumber = undefined
+
+// function addOne(callback) {
+//   fs.readFile('number.txt', function doneReading(err, fileContents) {
+//     myNumber = parseInt(fileContents)
+//     myNumber++
+//     callback()
+//   })
+// }
+
+// function logMyNumber() {
+//   console.log(myNumber)
+// }
+
+// addOne(logMyNumber)
+
+// 1: the code is parsed, which means if there are any syntax errors they would make the program break. During this initial phase there are 4 things that get defined: fs, myNumber, addOne, and logMyNumber. Note that these are just being defined, no functions have been called/invoked yet.
+// 2: When the last line of our program gets executed addOne gets invoked, getting passed in the logMyNumber function as callback, which is what we want to be called when addOne is complete. This immediately causes the asynchronous fs.readFile function to kick off. This part of the program takes a while to finish.
+// 3: with nothing to do, node idles for a bit as it waits for readFile to finish. If there was anything else to do during this time, node would be available for work.
+// 4: readFile finishes and calls its callback, doneReading, which then in turn increments the number and then immediately invokes the function that addOne passed in (its callback), logMyNumber.
+
+
