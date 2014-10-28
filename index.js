d1be2b94d37bb3261cd602a151412ff9d97c95d5
TKais <TiffanyTKaiser@gmail.com>
2014-10-28 09:07:12 -0700

    I think I actually understand callbacks

--- index.js
+++ index.js
@@ -215,4 +215,6 @@ http.createServer(app).listen(app.get('port'), function(){
 // asyncOperation ( params.., function ( err, returnValues.. ) {
 //    //This code gets run after the async operation gets run
 // });
-// --You will almost always want to follow the error callback convention, since most Node.js users will expect your project to follow them. The general idea is that the callback is the last parameter. The callback gets called after the function is done with all of its operations. Traditionally, the first parameter of the callback is the error value. If the function hits an error, then they typically call the callback with the first parameter being an Error object. If it cleanly exits, then they will call the callback with the first parameter being null and the rest being the return value(s).
+// --You will almost always want to follow the error callback convention, since most Node.js users will expect your project to follow them. The general idea is that the callback is the last parameter. The callback gets called after the function is done with all of its operations. Traditionally, the first parameter of the callback is the error value. If the function hits an error, then they typically call the callback with the first parameter being an Error object. If it cleanly exits, then they will call the callback with the first parameter being null and the rest being the return value(s)
+
+// --Callbacks are functions that use another function as a parameter so that it doesn't wait around for an actual return; it keeps doing other things
